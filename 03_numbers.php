43d050f4ff4808ccf6444a90b47303b5d6a4ba40
thatotheblackrose <thatothelancer@gmail.com>
2021-04-16 15:45:03 +0200

    15:44

--- 03_numbers.php
+++ 03_numbers.php
@@ -1,3 +1,51 @@
 <?php
-
 // Declaring numbers
+$a = 5;
+$b = 4;
+$c = 1.2;
+// Arithmetic operations
+echo ($a + $b) * $c .'<br>';
+echo $a - $b . '<br>';
+echo $a * $b . '<br>';
+echo $a / $b . '<br>';
+echo $a % $b . '<br>';
+// Assignment with math operators
+//$a += $b; echo $a. '<br>'; // $a = 9
+//$a -= $b; echo $a. '<br>'; //$a = 1
+//$a *= $b; echo $a. '<br>'; //$a = 28
+//$a /= $b; echo $a. '<br>'; //$a = 1.25
+// Increment operator
+ echo $a++.'<br>';
+ echo ++$a;
+
+// Decrement operator
+echo $a--. '<br>';
+echo --$a. '<br>';
+// Number checking functions
+is_float(1.25); //true
+is_double(1.25); // true
+is_int(5); //true
+is_numeric("3.45"); //true
+is_numeric("3g.45"); //false
+// Conversion
+$strNumber = '12.34.';
+$number = intval($strNumber); // float , int , floatval , intval
+var_dump($number);
+echo '<br>';
+
+// Number functions
+echo "abs(-15) " . abs(-15) . '<br>'; // oppsite value negative to positive and virse verse
+echo "pow(2, 3)" . pow(2,3) . '<br>'; // ?
+echo "sqrt(16)" . sqrt(16) . '<br>'; // squre root
+echo "max(2, 3, 9)" . max(2,3, 9) . '<br>'; // highest value
+echo "min(2,3,0, -1)" . min(2,3, 0, -1) . '<br>'; // lowest value
+echo "round(2.4)" . round(2.4) . '<br>'; // rounding off
+echo "round(2.6)" . round(2.6) . '<br>'; // rounding off
+echo "floor(2.8)" . floor(2.8) . '<br>'; // whole numbers only
+echo "ceil(2.4)" . ceil(2.4) . '<br>'; // ?
+
+// Formatting numbers
+$number = 123456789.12345;//
+ echo number_format($number, 2, '.', ',');
+
+// https://www.php.net/manual/en/ref.math.php
--- 04_strings.php
+++ 04_strings.php
@@ -1,11 +1,28 @@
 <?php
 
 // Create simple string
-
+$name = 'Tron';
+$string = 'Hello Black '.$name.'. i am alive ' ; // sentence stracture example 2
+$string2 = "Hello DARKNESS $name. is waiting for you"; // sentence stracture example 2
+echo $string. '<br>';
+echo $string2. '<br>';
 // String concatenation
-
+echo 'hey ', ' world ', 'Game is waiting', '<br>';
 // String functions
-
+$string = "          hello world   ";
+echo "1 - ". strlen($string) . '<br>' . PHP_EOL; // returns number of spaces between the qutoes{""} and the words
+echo "2 - ". trim($string) . '<br>' . PHP_EOL; // returns the variable
+echo "3 - ". ltrim($string) . '<br>' . PHP_EOL; // not sure ????? seems to return the same as above
+echo "4 - ". rtrim($string) . '<br>' . PHP_EOL; // not sure ????? seems to return the same as above{2}
+echo "5 - ". str_word_count($string) . '<br>' . PHP_EOL; // counts number of words or symbols
+echo "6 - ". strrev($string) . '<br>' . PHP_EOL; // returns the value backwards or in reverse
+echo "7 - ". strtoupper($string). '<br>' . PHP_EOL; // returns the value in capital letters
+echo "8 - ". strtolower($string). '<br>' . PHP_EOL; // returns the value in lower letters
+echo "9 - ". ucfirst($string). '<br>' . PHP_EOL;// not sure ???
+echo "10 - ". lcfirst($string). '<br>' . PHP_EOL;//
+echo "11 - ". ucwords($string). '<br>' . PHP_EOL;//
+echo "12 - ". strpos($string, 'world'). '<br>' . PHP_EOL;//
+echo "13 - " . stripos($string, 'word'). '<br>' . PHP_EOL; //
 // Multiline text and line breaks
 
 // Multiline text and reserve html tags
